c64a29dec0b229d7ea4b409b46ea2d92695b84c0
Sevinch-najmiddinova <najmiddinova2021@mail.ru>
2021-08-30 21:19:53 +0500

    add 2 js file and 1 img folder commit

---
+++ homework.js
@@ -0,0 +1,15 @@
+// let card = document.getElementsByTagName('div');
+let img = document.getElementsByTagName('img')
+
+for(let i = 0; i< img.length;i++){
+    setTimeout(() =>{
+        img[i].style.opacity = '1';
+       }, (i+1) *2000)
+    
+}
+let span = document.getElementsByTagName('span')
+for(let i = 0;i<span.length;i++){
+    setTimeout(() =>{
+        span[i].style.opacity = '1';
+       }, (i+1) *2000)
+}
